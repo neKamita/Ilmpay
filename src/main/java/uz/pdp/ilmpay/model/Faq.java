ba49d2bb6952a765076c6024dd22e7009905de41
cdy <nationalUB@gmail.com>
2025-03-13 20:27:36 +0500

    Optimization

--- src/main/java/uz/pdp/ilmpay/model/Faq.java
+++ src/main/java/uz/pdp/ilmpay/model/Faq.java
@@ -2,6 +2,7 @@ package uz.pdp.ilmpay.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Faq {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/uz/pdp/ilmpay/repository/FAQRepository.java
+++
@@ -1,7 +0,0 @@
-package uz.pdp.ilmpay.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.pdp.ilmpay.model.FAQ;
-
-public interface FAQRepository extends JpaRepository<FAQ, Long> {
-}
